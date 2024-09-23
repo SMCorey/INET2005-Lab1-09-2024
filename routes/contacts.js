cc53fc745340eac5aaa25688c200ef535018cd22
SMCorey <sloanc97@gmail.com>
2024-09-23 11:18:39 -0300

    updated routes and validation.

--- routes/contacts.js
+++ routes/contacts.js
@@ -35,18 +35,31 @@ router.get('/all', async (req, res) => {
 // Get a contact by id
 router.get('/get/:id', async (req, res) => {
     const id = req.params.id;
+
+    // validate if ID is a number
+    if (isNaN(id) ){
+        res.status(400).json({message: "Invalid ID"});
+        return;
+    }
     const contact = await prisma.contact.findUnique({
         where: {
             id: parseInt(id),
         }
     })
-        res.json(contact);
+       contact ? res.json(contact): res.status(404).json({message: "contact not found"});
 });
   
 // Create a contact (with multer)
 router.post('/create',upload.single('image'), async (req, res) => {
     const filename = req.file? req.file.filename : null;
     const {firstName, lastName,title, email, phone} = req.body
+
+    // verify required fields have value
+    if(!firstName || !lastName || !email || !phone){
+
+        // TODO: delete file // create method for deletion
+        return res.status(400).json({message: "Required fields must have a value."});
+    }
 
     const contact = await prisma.contact.create({
         data: {
@@ -63,13 +76,31 @@ router.post('/create',upload.single('image'), async (req, res) => {
 });
 
 // Update a contact by ID (with Multer)
+
 router.put('/update:id', upload.single('image'), (req, res) => {
+
+    // validate ID // return 404 if not found
+    
+    // capture inputs
+    // validate inputs
+
+    // make blank filename variable.
+    // if file is uploaded: get file name to save in db. delete old image file. Set the file name to new file name.
+    // if image file NOT uploaded: when updating record with prisma, set the filename to old file name.
+    
+    // update record in the db.
+
     const id = req.params.id;
     res.send('update route');
 });
 
 // Delete a contact
 router.delete('/delete:id', (req, res) => {
+
+    // validate ID. Return 404 if not found
+    // delete image
+    // delete record from DB.
+
     res.send('delete route');
 });
 
